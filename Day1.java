01bb1d0972cbd038a2341a08a4bdd8a87275199e
Rob Parker <jademonkey7@gmail.com>
2023-11-30 16:59:51 +0000

    prep for day 1

---
+++ Day1.java
@@ -0,0 +1,12 @@
+/**
+ * Day1
+ */
+public class Day1 {
+    static final String EXAMPLE_INPUT_1 = "E:/git/src/jademonkey/advent-of-code-2023/ExampleInputs/Day1";
+
+    static final String ACTUAL_INPUT_1 = "E:/git/src/jademonkey/advent-of-code-2023/Inputs/Day1";
+
+    public static void main(String[] args) {
+        System.out.println("TBC!");
+    }
+}
